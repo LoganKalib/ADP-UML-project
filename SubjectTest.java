2ec6c8a2c3edca46712f59de43b9167885df98f8
UzairRabin <127097004+UzairRabin@users.noreply.github.com>
2024-02-24 19:35:30 +0200

    SubjectTest

---
+++ SubjectTest.java
@@ -0,0 +1,39 @@
+package za.ac.cput;
+
+public class SubjectTest {
+    private final ConcurrentHashMap<Integer, Mark> marks = new ConcurrentHashMap<>();
+    private final Subject subject1 = new Subject("ITS","Information Systems",marks);
+    private final Subject subject2 = new Subject(subject1);
+
+    @Test
+    void testEqual(){
+        assertEquals(subject1.getSubjectCode(),subject2.getSubjectCode());
+
+    }
+    @Test
+    void testNotEqualIdentity(){
+        assertNotEquals(subject1.getMarks(),subject2.getMarks());
+
+    }
+
+    @Test
+    @Timeout(1)
+    void testMarks() throws InterruptedException {
+        Thread.sleep(999);
+        ConcurrentHashMap<Integer, Mark> testing = new ConcurrentHashMap<>();
+        testing.put(1,new Mark("ITS3", "Information Systems", 87));
+
+        subject1.setMarks(testing);
+
+        assertNotEquals(subject1.getMarks(),subject2.getMarks());
+    }
+
+    @Test
+    @Disabled("disable until lecturer are tested...")
+    void testLecturer(){
+        ConcurrentHashMap<Integer, Subject> subjects1 = subject1.getSubjects().get(1).getSubsTaught();
+        ConcurrentHashMap<Integer, Subject> subjects2 = subject2.getSubjects().get(1).getSubsTaught();
+        assertNotEquals(subjects1,subjects2);
+    }
+
+}
