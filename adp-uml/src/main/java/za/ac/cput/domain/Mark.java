e55443a76f01ed12e1b223fc0f9de54f94c8d67d
mohamedzouheir <126957679+mohamedzouheir@users.noreply.github.com>
2024-02-25 12:04:42 +0200

    Added mark test class and edited mark

--- adp-uml/src/main/java/za/ac/cput/domain/Mark.java
+++ adp-uml/src/main/java/za/ac/cput/domain/Mark.java
@@ -1,39 +1,39 @@
 package za.ac.cput.domain;
 
 public class Mark {
-    private double percentage;
-    private int marksPossible;
-    private int marksReceived;
+    private Double percentage;
+    private Integer marksPossible;
+    private Integer marksReceived;
     private String assessmentName;
     private String assessmentType;
 
     public Mark() {
     }
 
-    public Mark(int marksPossible, String assessmentName, String assessmentType) {
+    public Mark(Integer marksPossible, String assessmentName, String assessmentType) {
         this.marksPossible = marksPossible;
         this.assessmentName = assessmentName;
         this.assessmentType = assessmentType;
     }
 
-    public Mark(int marksPossible, int marksReceived, String assessmentName, String assessmentType) {
+    public Mark(Integer marksPossible, Integer marksReceived, String assessmentName, String assessmentType) {
         this.marksPossible = marksPossible;
         this.marksReceived = marksReceived;
         this.assessmentName = assessmentName;
         this.assessmentType = assessmentType;
-        percentage = marksReceived/marksPossible * 100;
+        percentage = marksReceived.doubleValue()/marksPossible.doubleValue() * 100;
 
     }
 
-    public double getPercentage() {
-        if(marksPossible && marksReceived != null){
-            percentage = marksReceived /marksPossible * 100;
+    public Double getPercentage() {
+        if(marksPossible != null && marksReceived != null){
+            percentage = marksReceived.doubleValue()/marksPossible.doubleValue() * 100;
             return percentage;
         }
         return null;
     }
 
-    public int getMarksPossible() {
+    public Integer getMarksPossible() {
         return marksPossible;
     }
 
@@ -41,11 +41,12 @@ public class Mark {
         this.marksPossible = marksPossible;
     }
 
-    public int getMarksReceived() {
+    public Integer getMarksReceived() {
         return marksReceived;
     }
 
-    public void setMarksReceived(int marksReceived) {
+    public void setMarksReceived(Integer marksReceived) {
+        percentage = marksReceived.doubleValue()/this.marksPossible.doubleValue() * 100;
         this.marksReceived = marksReceived;
     }
 
